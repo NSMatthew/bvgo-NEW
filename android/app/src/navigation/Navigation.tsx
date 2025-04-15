dd50d0389c6479e147a1ee7356ad05e1449f40bd
NSMatthew <matthewnovansidharta31@gmail.com>
2025-04-15 18:13:27 +0800

    add navigatio

---
+++ android/app/src/navigation/Navigation.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../types/types';
+
+import Splash from '../screen/Splash';
+import Login from '../screen/Login';
+// import Home from '../screen/Home'; // nanti kalau udah ada
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const linking = {
+  prefixes: ['myapp://'],
+  config: {
+    screens: {
+      Splash: 'splash',
+      Login: 'login',
+      Home: 'home/:userId',
+    },
+  },
+};
+
+const Navigation = () => {
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator
+        initialRouteName="Splash"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Home" component={Home} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;

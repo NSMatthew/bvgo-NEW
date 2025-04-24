c45871564d17c088ec15d3ec370575f9f92a96fb
NSMatthew <matthewnovansidharta31@gmail.com>
2025-04-24 09:49:18 +0800

    add typography

---
+++ src/styles/typography.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export const Typography = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  subheading: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#333',
+  },
+  body: {
+    fontSize: 14,
+    fontWeight: 'normal',
+    color: '#444',
+  },
+});

1a0bb1cdd3f1bfacf2bad34a1e21386ef07cdc16
NSMatthew <matthewnovansidharta31@gmail.com>
2025-04-25 16:18:39 +0800

    proses fix supabase.ts (part 1) mennggunakan react-native-config

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { config } from 'dotenv'
+import Config from 'react-native-config';  // Import react-native-config
+import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-config()
-
-const supabaseUrl = process.env.SUPABASE_URL || ''
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || ''
+const supabaseUrl = Config.SUPABASE_URL;
+const supabaseAnonKey = Config.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
@@ -15,4 +12,4 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     persistSession: true,
     detectSessionInUrl: false,
   },
-})
+});

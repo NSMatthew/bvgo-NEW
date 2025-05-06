b04c8b7629c8e113062fac9c6e0322914fc6dbf0
NSMatthew <matthewnovansidharta31@gmail.com>
2025-05-06 15:36:46 +0800

    update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,15 @@
-import Config from 'react-native-config';  // Import react-native-config
+import Config from 'react-native-config'; //load file env
 import { createClient } from '@supabase/supabase-js';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const supabaseUrl = Config.SUPABASE_URL;
 const supabaseAnonKey = Config.SUPABASE_ANON_KEY;
 
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables. Check .env and react-native-config setup.');
+}
+
+// Inisialisasi Supabase Client
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,

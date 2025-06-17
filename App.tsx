2b7e92f1ad29a35909efb86ca197d8e24912f62f
NSMatthew <matthewnovansidharta31@gmail.com>
2025-06-17 12:20:48 +0800

    update sintaks path ke Navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import Navigation from '../src/navigation/Navigation';
+import Navigation from './src/navigation/Navigation';
 
 const App = () => {
   return (
